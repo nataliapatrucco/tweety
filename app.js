67b77e92673cfc77d8d19443d61f5a67ba98b680
nataliapatrucco <patrucconatalia@gmail.com>
2019-09-25 14:34:54 -0300

    modificado

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 const express = require("express");
-const tweetyApp = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 const nunjucks = require("nunjucks");
+const routes = require("./routes");
+app.use("/", routes);
 
-const locals = {
-  title: "An Example",
-  people: [{ name: "Gandalf" }, { name: "Frodo" }, { name: "Hermione" }]
-};
-
-tweetyApp.set("view engine", "html"); // hace que res.render funcione con archivos html
-tweetyApp.engine("html", nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+app.set("view engine", "html"); // hace que res.render funcione con archivos html
+app.engine("html", nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
 nunjucks.configure("views", { noCache: true });
 
-tweetyApp.use("/", function(req, res, next) {
+app.use("/", function(req, res, next) {
   console.log("Request recibida");
   next();
 });
 
-tweetyApp.use("/special/", function(req, res, next) {
+app.use("/special/", function(req, res, next) {
   console.log("Entro a Special");
   next();
 });
 
-tweetyApp.get("/", function(req, res) {
-  res.render("index", locals, (err, output) => {
-    if (err) throw err;
-    res.send(output);
-  });
-});
+app.use(express.static("public"));
 
-tweetyApp.get("/", function(req, res) {
-  return res.send("<h1>Hello World!!!!!!!</h1>");
-});
-tweetyApp.get("/special/", function(req, res) {
-  return res.send("<h1>llegaste a un area especial</h1>");
-});
+app.post("/", function(req, res) {});
+app.put("/", function(req, res) {});
+app.delete("/", function(req, res) {});
 
-tweetyApp.post("/", function(req, res) {});
-tweetyApp.put("/", function(req, res) {});
-tweetyApp.delete("/", function(req, res) {});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-tweetyApp.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// const locals = {
+//   title: "An Example",
+//   people: [{ name: "Gandalf" }, { name: "Frodo" }, { name: "Hermione" }]
+// };
+
+// app.get("/", function(req, res) {
+//   res.render("index", locals, (err, output) => {
+//     if (err) throw err;
+//     res.send(output);
+//   });
+// });
+
+// app.get("/", function(req, res) {
+//   return res.send("<h1>Hello World!!!!!!!</h1>");
+// });
+// app.get("/special/", function(req, res) {
+//   return res.send("<h1>llegaste a un area especial</h1>");
+// });
